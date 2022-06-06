c870e39f47440d3b30287f37898d75280f8248ed
十百千 <x1001000@users.noreply.github.com>
2022-06-06 14:14:03 +0800

    locked folder

--- patch.py
+++ patch.py
@@ -1,3 +1,3 @@
 import glob, os
-for avi_tmp in glob.glob('Result\*\*.avi.tmp'):
+for avi_tmp in glob.glob('Result.{2559a1f2-21d7-11d4-bdaf-00c04f60b9f0}\*\*.avi.tmp'):
     os.system(f'del {avi_tmp}')

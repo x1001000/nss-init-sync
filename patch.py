03ad894d44277372aafac1d431e749e1f6756158
Phil Peng <x1001000@gmail.com>
2022-11-15 19:46:53 +0800

    fix path

--- patch.py
+++ patch.py
@@ -1,9 +1,9 @@
 import glob, os
 for avi_tmp in glob.glob(os.path.join(os.getcwd(), 'Result\*\*.avi.tmp')):
     os.system(f'del {avi_tmp}')
-with open('sync.py', 'r') as f:
+with open(os.path.join(os.getcwd(), 'sync.py'), 'r') as f:
     lines = f.readlines()
-with open('sync.py', 'w') as f:
+with open(os.path.join(os.getcwd(), 'sync.py'), 'w') as f:
     lines[36] = "        size += sum([os.path.getsize(os.path.join(root, name)) for name in files if 'avi' not in name])\n"
     lines[63] = "    shutil.copytree(ori_dir_path, target_dir_path, ignore=shutil.ignore_patterns('*avi*'))\n"
     f.writelines(lines)
